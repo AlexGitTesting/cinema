4db146570b1872b5c87a228b5a41f1598e0857e1
yefremov.a <yefremov.a@oranges.guru>
2022-09-10 23:43:52 +0300

    domain config

---
+++ src/main/java/com/example/cinema/config/DomainConfig.java
@@ -0,0 +1,49 @@
+package com.example.cinema.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@EnableJpaAuditing
+@Configuration(proxyBeanMethods = false)
+@EntityScan({"com.example.cinema.domain"})
+@EnableJpaRepositories(basePackages = {"com.example.cinema.dao"})
+public class DomainConfig implements WebMvcConfigurer {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasenames("classpath:/i18n/i18n_local_message");
+        source.setDefaultEncoding("UTF-8");
+        return source;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+        acceptHeaderLocaleResolver.setDefaultLocale(new Locale("uk"));
+        return acceptHeaderLocaleResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    private LocaleChangeInterceptor localeChangeInterceptor() {
+        final LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setIgnoreInvalidLocale(true);
+        return localeChangeInterceptor;
+    }
+
+}
