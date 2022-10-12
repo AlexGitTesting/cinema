9bca32c032be493f29d01bbe537a855c2aff2b9e
yefremov.a <yefremov.a@oranges.guru>
2022-10-12 12:07:57 +0300

    @OpenAPIDefinition

--- src/main/java/com/example/cinema/CinemaApplication.java
+++ src/main/java/com/example/cinema/CinemaApplication.java
@@ -1,11 +1,13 @@
 package com.example.cinema;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Cinema application Api", description = "Representation of the service which help you book seats in the cinema"))
 public class CinemaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
     }
